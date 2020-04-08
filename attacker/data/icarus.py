dc7623bf07e9d1a5bdb0826ce2a122763aec3f96
Clarissa April Liana <clarissa.liana@tokopedia.com>
2020-04-08 15:54:06 +0700

    add data, task icarus.py and dashboard icarus grafana

--- attacker/data/icarus.py
+++ attacker/data/icarus.py
@@ -1,25 +1,51 @@
 STATIC_DATA = {
     "home": [
         "/home",
+        "/home?source=asd",
+    ],
+    "broadcast": [
+        "/broadcast",
+    ],
+    "produk_toko_cabang": [
+        "/produk-toko-cabang",
+    ],
+    "dekorasi_toko": [
+        "/dekorasi-toko",
     ],
     "manage_product": [
         "/manage-product",
+        "/manage-product#",
+        "/manage-product?tab=1",
+        "/manage-product?tab=2",
         "/manage-product?isStockOnly=true",
+        "/manage-product?nref=pdlsthead",
     ],
     "manage_campaign": [
         "/manage-campaign",
-    ],
-    "my_campaign": [
+        "/manage-campaign?opencriteria=true",
         "/manage-campaign/my-campaign",
-    ],
-    "manage_campaign_detail": [
-        "/manage-campaign/test-pqr",
         "/manage-campaign/campaign-slug",
+        "/manage-campaign/test-pqr",
+    ],
+    "myshop_order": [
+        "/myshop_order",
+        "/myshop_order?status=confirm_shipping",
+        "/myshop_order?status=new_order",
+        "/myshop_order?status=in_shipping",
+        "/myshop_order?status=done",
+        "/myshop_order?status=order_canceled",
+        "/myshop_order?status=all_order?search=ibu",
+        "/myshop_order?status=confirm_shipping&search=Itaewon+Class+Danbam+Keychain",
+    ],
+    "promo": [
+        "/promo",
     ],
     "statistic_overview": [
         "/statistic/overview",
     ],
-    "promo": [
-        "/promo",
+    "unknown": [
+        "/unknown",
+        "/promos",
+        "/homes",
     ],
 }
