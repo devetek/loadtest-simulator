3f898fefb1f0cd93a6c033a87de82a46ba5d3065
Clarissa April Liana <clarissa.liana@tokopedia.com>
2020-04-08 08:55:05 +0700

    add icarus.py to data and task

---
+++ attacker/data/icarus.py
@@ -0,0 +1,25 @@
+STATIC_DATA = {
+    "home": [
+        "/home",
+    ],
+    "manage_product": [
+        "/manage-product",
+        "/manage-product?isStockOnly=true",
+    ],
+    "manage_campaign": [
+        "/manage-campaign",
+    ],
+    "my_campaign": [
+        "/manage-campaign/my-campaign",
+    ],
+    "manage_campaign_detail": [
+        "/manage-campaign/test-pqr",
+        "/manage-campaign/campaign-slug",
+    ],
+    "statistic_overview": [
+        "/statistic/overview",
+    ],
+    "promo": [
+        "/promo",
+    ],
+}
