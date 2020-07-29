d04a39380b83ee4c3ca21b3a57f164aef2b99a22
Clarissa <clarissa.liana@tokopedia.com>
2020-07-29 10:26:58 +0700

    add wp-seller RPS for Grafana (#7)
    * add wp-seller RPS for Grafana
    
    * deleting some data to test

---
+++ attacker/data/wpseller.py
@@ -0,0 +1,87 @@
+STATIC_DATA = {
+    "home": [
+        "/edu",
+        "/edu?source=tokopedia%20home",
+    ],
+    "articles": [
+        "/edu/articles",
+        "/edu/articles/latest",
+        "/edu/articles/popular",
+    ],
+    "article_detail": [
+        "/edu/konten-pilihan-hari-ini",
+        "/edu/newsletter-juli-2020",
+        "/edu/waktu-indonesia-belanja",
+    ],
+    "admin_main": [
+        "/edu/wp-admin",
+        "/edu/wp-admin/users.php",
+        "/edu/wp-admin/admin-ajax.php",
+        "/edu/wp-admin/edit.php",
+        "/edu/wp-admin/edit.php?post_type=banner-login",
+        "/edu/wp-admin/plugin.php",
+    ],
+    "admin_hide": [
+        "/edu/nest-tkpd",
+        "/edu/nest-tkpd/users.php",
+        "/edu/nest-tkpd/admin-ajax.php",
+        "/edu/nest-tkpd/edit.php",
+        "/edu/nest-tkpd/edit.php?post_type=page",
+        "/edu/nest-tkpd/plugin.php",
+    ],
+    "admin_login": [
+        "/edu/wp-login",
+    ],
+    "wp_content": [
+        "/edu/wp-content/themes/tkpd-seller/src/images/bebasOngkir.min.png",
+        "/edu/wp-content/themes/tkpd-seller/src/images/pm/favorite-icon.min.png",
+        "/edu/wp-content/plugins/query-monitor/assets/query-monitor.js?ver=1570159823",
+        "/edu/wp-content/themes/tkpd-seller/style.css?ver=5.1.1",
+    ],
+    "wp_includes": [
+        "/edu/wp-includes/css/dashicons.min.css?ver=5.1.1",
+        "/edu/wp-includes/css/dist/block-library/style.min.css?ver=5.1.1",
+    ],
+    "wp_json": [
+        "/edu/wp-json/wp/v2/categories",
+        "/edu/wp-json/wp/v2/posts",
+        "/edu/wp-json/wp/v2/posts/?per_page=1&_embed",
+    ],
+    "microsite_event": [
+        "/edu/event-calendar",
+    ],
+    "microsite_kembangkan_usaha": [
+        "/edu/tips-kembangkan-usaha",
+    ],
+    "microsite_mulai_berjualan": [
+        "/edu/tips-mulai-berjualan",
+    ],
+    "microsite_layanan": [
+        "/edu/layanan",
+    ],
+    "microsite_official_store": [
+        "/edu/official-store",
+    ],
+    "microsite_order_prioritas": [
+        "/edu/order-prioritas",
+    ],
+    "microsite_power_merchant": [
+        "/edu/power-merchant",
+    ],
+    "microsite_tingkatkan_penjualan": [
+        "/edu/tips-tingkatkan-penjualan",
+    ],
+    "microsite_topads": [
+        "/edu/about-topads",
+        "/edu/about-topads/broadcast-chat",
+        "/edu/about-topads/iklan",
+        "/edu/about-topads/voucher-toko",
+    ],
+    "microsite_topbot": [
+        "/edu/topbot",
+    ],
+    "else": [
+        "/edux",
+        "/apaajaboleh",
+    ],
+}
