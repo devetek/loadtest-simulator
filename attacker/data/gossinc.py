1ecbd32bc34e984d8c8ad5dee2e807336f8d673a
baguskah <bagus.utama@BA-00015935.local>
2021-06-18 15:34:56 +0700

    add referer for prometheus

---
+++ attacker/data/gossinc.py
@@ -0,0 +1,5 @@
+STATIC_DATA = {
+    "assets": [
+        "/assets/css",
+    ],
+}
---
+++ attacker/task/gossinc.py
@@ -0,0 +1,14 @@
+from locust import TaskSet, task
+from data.gossinc import STATIC_DATA
+
+
+class Behavior(TaskSet):
+    @task(1)
+    def index(self):
+        len_data = len(STATIC_DATA["assets"])
+        if len_data == 0:
+            pass
+
+        for i in range(len_data):
+            self.client.get(STATIC_DATA["assets"][i])
+
